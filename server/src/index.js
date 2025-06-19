88ff29ae8c0210d180877098065829aea2914a01
Okan Sönmez <okso2004@gmail.com>
2025-06-19 16:13:08 +0200

    add dummy code and fix path

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the public directory
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../../public')));
 
 // Start HTTP server
 const server = app.listen(PORT, () => {
