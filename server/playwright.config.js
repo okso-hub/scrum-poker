ff26b34197ff3c0f236182ddb64f5d4133289521
Nico Wrede <nico.wrede@sap.com>
2025-07-09 19:18:29 +0200

    use Chrome only

--- server/playwright.config.js
+++ server/playwright.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   use: {
+    browserName: 'chromium',
+    channel: 'chrome',
     baseURL: 'http://localhost:3000',
     headless: process.env.CI_PIPELINE === 'true' ? true : false,
     viewport: { width: 1280, height: 720 },
