19ecaf5f7813e2b3d7002e087aa39ee9e6b831cc
Nico Wrede <nico.wrede@sap.com>
2025-07-09 19:06:12 +0200

    Adjust e2e test

--- server/playwright.config.js
+++ server/playwright.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   use: {
     baseURL: 'http://localhost:3000',
-    headless: true,
+    headless: false,
     viewport: { width: 1280, height: 720 },
   },
   webServer: {
