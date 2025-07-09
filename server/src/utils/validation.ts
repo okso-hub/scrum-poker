ed0a4424b1638ae08288c7cf8dd8024fecc5be80
Nico Wrede <nico.wrede@sap.com>
2025-07-09 21:03:03 +0200

    RegEx checks for username & change error message attribute name

--- server/src/utils/validation.ts
+++ server/src/utils/validation.ts
@@ -1,4 +1,8 @@
 import { BadRequestError } from "../types/index.js";
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+const userRegex = require("../../assets/userRegEx.json") as { patterns: string[] };
 
 export function validateRoomId(roomId: any): number {
   if (!roomId) {
@@ -12,4 +16,13 @@ export function validateRoomId(roomId: any): number {
   }
   
   return numericRoomId;
+}
+
+export function validateUsername(userName: string): boolean {
+  for (const pat of userRegex.patterns) {
+    if (new RegExp(pat, ).test(userName)) {
+      return false;
+    }
+  }
+  return true;
 }
